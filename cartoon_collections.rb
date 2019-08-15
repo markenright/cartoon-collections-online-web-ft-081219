036d605ece299acfebce2db4a368dcf4a41d94d9
Mark Enright <enright.mark@gmail.com>
2019-08-15 19:07:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |word|
-    cheese_types.include?(word)
+    puts cheese_types.include?(word)
   
   end
     
